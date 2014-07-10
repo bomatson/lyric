8cff72292799e4b38c815336243773120404f42f
bomatson <bobby@carbonfive.com>
2014-07-09 17:34:57 -0700

    create the lyric factory and search bar;

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
+  .controller('mainCtrl', ['$scope', '$http', 'Lyric', function($scope, $http, Lyric) {
+    $scope.search = function(query) {
+      $http({
+        url: 'https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/track.search',
+        method: 'GET',
+        params: {  page_size: 5, page: 1, q: query || '' }
+      }).success( function(data) {
+        $scope.results = Lyric.parse(data)
+      });
+    }
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,13 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  factory('Lyric', function(){
+      var lyrics = {}
+
+      lyrics.parse = function(data) {
+        return data
+      }
+
+      return lyrics
+    }
+  );
